0d149f042b6ed68be8fa0ba6ea04db65b64cb020
pralhadshinde99 <pralhadshinde299@gmail.com>
2024-03-29 09:40:56 +0530

    add docker

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
-# Use the official Python image as base
-FROM python:3.9
+FROM python:3.9-slim
 
-# Set environment variables
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
-
-# Set the working directory in the container
 WORKDIR /app
 
-# Install dependencies
-COPY requirements.txt .
+COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of your application code to the container
 COPY . .
 
-# Command to run your application
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,3 +1,97 @@
+# using sqlaalchemy
+
+from fastapi import FastAPI
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from fastapi import Query
+from fastapi import HTTPException
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.post("/items/")
+@app.post("/items/", response_model=dict)
+def create_item(name: str, description: str):
+    db = SessionLocal()
+    db_item = Item(name=name, description=description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return {"message": "Item posted successfully"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+
+@app.put("/items/{item_id}", response_model=dict)
+def update_item(item_id: int, name: str, description: str):
+    db = SessionLocal()
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db_item.name = name
+    db_item.description = description
+    db.commit()
+    return {"message": "Item updated successfully"}
+
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    db = SessionLocal()
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.delete(db_item)
+    db.commit()
+    return {"message": "Item deleted successfully"}
+
+
+@app.get("/items/")
+def get_all_item():
+    db = SessionLocal()
+    item = db.query(Item).all()
+    return item
+
+
+@app.get("/items/")
+def get_all_item(name: str = Query(None, title="Name", description="Search by item name or description"),
+                 description: str = Query(None, title="Description", description="Search by item description")):
+    db = SessionLocal()
+    query = db.query(Item)
+    if name:
+        query = query.filter(Item.name.ilike(f"%{name}%"))
+    if description:
+        query = query.filter(Item.description.ilike(f"%{description}%"))
+    items = query.all()
+    return items
+
+
+
 # from fastapi import FastAPI, HTTPException
 # from pydantic import BaseModel
 #
@@ -51,95 +145,3 @@
 #     import uvicorn
 #
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-# using sqlaalchemy
-
-from fastapi import FastAPI
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from fastapi import Query
-from fastapi import HTTPException
-from dotenv import load_dotenv
-import os
-#repo
-load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-#model
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
-
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-
-@app.post("/items/")
-@app.post("/items/", response_model=dict)
-def create_item(name: str, description: str):
-    db = SessionLocal()
-    db_item = Item(name=name, description=description)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return {"message": "Item posted successfully"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    db = SessionLocal()
-    item = db.query(Item).filter(Item.id == item_id).first()
-    if item is None:
-        raise HTTPException(status_code=404, detail="Item not foundddd")
-    return item
-
-
-@app.put("/items/{item_id}", response_model=dict)
-def update_item(item_id: int, name: str, description: str):
-    db = SessionLocal()
-    db_item = db.query(Item).filter(Item.id == item_id).first()
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    db_item.name = name
-    db_item.description = description
-    db.commit()
-    return {"message": "Item updated successfully"}
-
-
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    db = SessionLocal()
-    db_item = db.query(Item).filter(Item.id == item_id).first()
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    db.delete(db_item)
-    db.commit()
-    return {"message": "Item deleted successfully"}
-
-
-@app.get("/items/")
-def get_all_item():
-    db = SessionLocal()
-    item = db.query(Item).all()
-    return item
-
-
-@app.get("/items/")
-def get_all_item(name: str = Query(None, title="Name", description="Search by item name or description"),
-                 description: str = Query(None, title="Description", description="Search by item description")):
-    db = SessionLocal()
-    query = db.query(Item)
-    if name:
-        query = query.filter(Item.name.ilike(f"%{name}%"))
-    if description:
-        query = query.filter(Item.description.ilike(f"%{description}%"))
-    items = query.all()
-    return items
